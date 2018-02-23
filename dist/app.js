34d74e65791c521e4dfaf1f988376bf2b291c270
tocausan <tocausan@gmail.com>
2018-02-23 07:50:27 +0100

    business card

--- dist/app.js
+++ dist/app.js
@@ -144,42 +144,6 @@ angular.module('footer', [])
     });
 'use strict';
 
-// init module
-angular.module('skills', ['ngMaterial'])
-    .controller('skillsCtrl', function ($scope, $mdDialog) {
-
-        // get data
-        ($scope.getData = function () {
-            // get media data
-            var skillFile = 'assets/json/skill.json';
-            $scope.languages = [];
-            $scope.skills = [];
-            // promise
-            new Promise(function (resolve, reject) {
-                // New XMLHttpRequest
-                var xhr = new XMLHttpRequest();
-                xhr.open('get', skillFile, true);
-                xhr.responseType = 'json';
-                xhr.onload = function () {
-                    if (xhr.status == 200) {
-                        // Success
-                        resolve(xhr.response);
-                        // Set total pages
-                        $scope.$apply(function () {
-                            $scope.languages = xhr.response.languages;
-                            $scope.skills = xhr.response.skills;
-                        });
-                    } else {
-                        // Error
-                        reject(xhr.status);
-                    }
-                };
-                xhr.send();
-            });
-        })();
-    });
-'use strict';
-
 angular.module('home', [])
     .controller('homeCtrl', function ($scope) {
 
@@ -214,4 +178,41 @@ angular.module('home', [])
                     break;
             }
         };
+    });
+
+'use strict';
+
+// init module
+angular.module('skills', ['ngMaterial'])
+    .controller('skillsCtrl', function ($scope, $mdDialog) {
+
+        // get data
+        ($scope.getData = function () {
+            // get media data
+            var skillFile = 'assets/json/skill.json';
+            $scope.languages = [];
+            $scope.skills = [];
+            // promise
+            new Promise(function (resolve, reject) {
+                // New XMLHttpRequest
+                var xhr = new XMLHttpRequest();
+                xhr.open('get', skillFile, true);
+                xhr.responseType = 'json';
+                xhr.onload = function () {
+                    if (xhr.status == 200) {
+                        // Success
+                        resolve(xhr.response);
+                        // Set total pages
+                        $scope.$apply(function () {
+                            $scope.languages = xhr.response.languages;
+                            $scope.skills = xhr.response.skills;
+                        });
+                    } else {
+                        // Error
+                        reject(xhr.status);
+                    }
+                };
+                xhr.send();
+            });
+        })();
     });
