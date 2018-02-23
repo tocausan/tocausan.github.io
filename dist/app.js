73d64ff9000a7890dcfd2b1da9fb10268f5811db
tocausan <tocausan@gmail.com>
2018-02-23 07:34:01 +0100

    business card

--- dist/app.js
+++ dist/app.js
@@ -137,44 +137,6 @@ function DialogCtrl($scope, $mdDialog, item) {
 };
 'use strict';
 
-angular.module('home', [])
-    .controller('homeCtrl', function ($scope) {
-
-        $scope.move = ($event, direction) => {
-            const directions = ['top', 'right', 'bottom', 'left'],
-                //element = $event.currentTarget;
-                element = angular.element(document.querySelector('.home-section'))[0];
-
-            direction = !element.classList.contains(directions[0]) &&
-            !element.classList.contains(directions[1]) &&
-            !element.classList.contains(directions[2]) &&
-            !element.classList.contains(directions[3]) ? direction : null;
-
-            switch (direction) {
-                case directions[0]:
-                    element.classList.add(directions[0]);
-                    break;
-                case directions[1]:
-                    element.classList.add(directions[1]);
-                    break;
-                case directions[2]:
-                    element.classList.add(directions[2]);
-                    break;
-                case directions[3]:
-                    element.classList.add(directions[3]);
-                    break;
-                default:
-                    element.classList.contains(directions[0]) ? element.classList.remove(directions[0]) : null;
-                    element.classList.contains(directions[1]) ? element.classList.remove(directions[1]) : null;
-                    element.classList.contains(directions[2]) ? element.classList.remove(directions[2]) : null;
-                    element.classList.contains(directions[3]) ? element.classList.remove(directions[3]) : null;
-                    break;
-            }
-        };
-    });
-
-'use strict';
-
 angular.module('footer', [])
     .controller('footerCtrl', function ($scope) {
         $scope.name = 'tocausan';
@@ -215,4 +177,41 @@ angular.module('skills', ['ngMaterial'])
                 xhr.send();
             });
         })();
+    });
+'use strict';
+
+angular.module('home', [])
+    .controller('homeCtrl', function ($scope) {
+
+        $scope.move = ($event, direction) => {
+            const directions = ['top', 'right', 'bottom', 'left'],
+                //element = $event.currentTarget;
+                element = angular.element(document.querySelector('.home-section'))[0];
+
+            direction = !element.classList.contains(directions[0]) &&
+            !element.classList.contains(directions[1]) &&
+            !element.classList.contains(directions[2]) &&
+            !element.classList.contains(directions[3]) ? direction : null;
+
+            switch (direction) {
+                case directions[0]:
+                    element.classList.add(directions[0]);
+                    break;
+                case directions[1]:
+                    element.classList.add(directions[1]);
+                    break;
+                case directions[2]:
+                    element.classList.add(directions[2]);
+                    break;
+                case directions[3]:
+                    element.classList.add(directions[3]);
+                    break;
+                default:
+                    element.classList.contains(directions[0]) ? element.classList.remove(directions[0]) : null;
+                    element.classList.contains(directions[1]) ? element.classList.remove(directions[1]) : null;
+                    element.classList.contains(directions[2]) ? element.classList.remove(directions[2]) : null;
+                    element.classList.contains(directions[3]) ? element.classList.remove(directions[3]) : null;
+                    break;
+            }
+        };
     });
